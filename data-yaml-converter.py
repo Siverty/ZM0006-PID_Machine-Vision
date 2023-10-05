bcc9bcac9a4130415b5c3f6bc193b0e36b7fdaad
Siverty <justinschots13@gmail.com>
2023-10-05 15:05:31 +0200

    now scans every .txt file supposedly

--- data-yaml-converter.py
+++ data-yaml-converter.py
@@ -14,21 +14,27 @@ original_class_mapping = {class_name: idx for idx, class_name in enumerate(origi
 # Create a mapping from class indices in the updated data.yaml to their corresponding names
 updated_class_mapping = {idx: class_name for idx, class_name in enumerate(updated_data['names'])}
 
-# Process each .txt file
-for txt_file_name in ['file1.txt', 'file2.txt', 'file3.txt']:  # Replace with your file names
-    with open(txt_file_name, 'r') as file:
-        lines = file.readlines()
+# Directory containing the .txt files
+txt_folder = 'datasets/dataset3/train/labels'
 
-    new_lines = []
-    for line in lines:
-        parts = line.strip().split(' ')
-        if len(parts) >= 1:
-            original_class_idx = int(parts[0])
-            if original_class_idx in original_class_mapping:
-                updated_class_name = updated_class_mapping[original_class_mapping[original_class_idx]]
-                parts[0] = str(updated_data['names'].index(updated_class_name))
-                new_lines.append(' '.join(parts))
+# Process each .txt file in the folder
+for txt_file_name in os.listdir(txt_folder):
+    if txt_file_name.endswith('.txt'):
+        txt_file_path = os.path.join(txt_folder, txt_file_name)
 
-    # Write the updated lines back to the .txt file
-    with open(txt_file_name, 'w') as file:
-        file.write('\n'.join(new_lines))
+        with open(txt_file_path, 'r') as file:
+            lines = file.readlines()
+
+        new_lines = []
+        for line in lines:
+            parts = line.strip().split(' ')
+            if len(parts) >= 1:
+                original_class_idx = int(parts[0])
+                if original_class_idx in original_class_mapping:
+                    updated_class_name = updated_class_mapping[original_class_mapping[original_class_idx]]
+                    parts[0] = str(updated_data['names'].index(updated_class_name))
+                    new_lines.append(' '.join(parts))
+
+        # Write the updated lines back to the .txt file
+        with open(txt_file_path, 'w') as file:
+            file.write('\n'.join(new_lines))
