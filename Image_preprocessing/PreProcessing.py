39c1b782c7db7370ec5437c61b90d9716a6418a8
Siverty <justinschots13@gmail.com>
2023-10-27 10:55:59 +0200

    Added pdf_to_png_converter

--- Image_preprocessing/PreProcessing.py
+++ Image_preprocessing/PreProcessing.py
@@ -3,11 +3,14 @@ from blur import blur_images
 from erode import erode_images
 from noise import add_noise
 from mosaic import mosaic_images
+from pdf_to_png_converter import pdf_to_png_converter
 import os
 
 # Set train and label folder paths
 train_folder = "./datasets/Labels_PID/images/train"
 label_folder = "./datasets/Labels_PID/labels/train"
+check_for_pdf_in_folder = "Test/Original_PID"
+copy_folder = "Test/Original_PID_PDF"
 
 # Loop through images in train folder
 for filename in os.listdir(train_folder):
@@ -22,3 +25,6 @@ for filename in os.listdir(train_folder):
 
     # Apply mosaic
     img = mosaic_images(train_folder, label_folder)
+
+    # Convert pdf to png
+    img = pdf_to_png_converter(check_for_pdf_in_folder, copy_folder)
