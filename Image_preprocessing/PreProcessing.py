4fb09bd1e8476b165142c58eb3b5ba875709c68f
Siverty <justinschots13@gmail.com>
2023-10-27 12:56:17 +0200

    Pipeline now works as intended

--- Image_preprocessing/PreProcessing.py
+++ Image_preprocessing/PreProcessing.py
@@ -7,15 +7,15 @@ from pdf_to_png_converter import pdf_to_png_converter
 import os
 
 # Set train and label folder paths
-train_folder = ".\datasets\Labels_PID\images\train"
-label_folder = ".\datasets\Labels_PID\labels\train"
-check_for_pdf_in_folder = "test\Original_PID"
-copy_folder = "test\Original_PID_PDF"
+train_folder = './datasets/Labels_PID/images/train'
+label_folder = './datasets/Labels_PID/labels/train'
+input_folder = 'test\Converted_PID_JPEG'
+output_folder = 'test\Original_PID'
 
 # Loop through images in train folder
 for filename in os.listdir(train_folder):
     # Convert pdf to png
-    img = pdf_to_png_converter(check_for_pdf_in_folder, copy_folder)
+    pdf_to_png_converter(input_folder, output_folder)
 
     # Apply blur
     img = blur_images(train_folder, label_folder)
--- Image_preprocessing/pdf_to_png_converter.py
+++ Image_preprocessing/pdf_to_png_converter.py
@@ -2,30 +2,38 @@ import os
 import fitz  # PyMuPDF
 from PIL import Image
 
-# Specify the input and output folders
-input_folder = 'test\Converted_PID_JPEG'
-output_folder = 'test\Original_PID'
+def pdf_to_png_converter(input_folder, output_folder):
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
 
-# Create the output folder if it doesn't exist
-os.makedirs(output_folder, exist_ok=True)
+    # Iterate through the PDF files in the input folder
+    for filename in os.listdir(input_folder):
+        if filename.endswith('.pdf'):
+            pdf_file = os.path.join(input_folder, filename)
 
-# Iterate through the PDF files in the input folder
-for filename in os.listdir(input_folder):
-    if filename.endswith('.pdf'):
-        pdf_file = os.path.join(input_folder, filename)
+            # Open the PDF file using PyMuPDF
+            pdf_document = fitz.open(pdf_file)
 
-        # Open the PDF file using PyMuPDF
-        pdf_document = fitz.open(pdf_file)
+            # Iterate through the pages and save each page as a PNG
+            for page_num in range(pdf_document.page_count):
+                page = pdf_document.load_page(page_num)
 
-        # Iterate through the pages and save each page as a PNG
-        for page_num in range(pdf_document.page_count):
-            page = pdf_document.load_page(page_num)
-            image = page.get_pixmap()
-            png_file = os.path.splitext(filename)[0] + f'_page{page_num + 1}.png'
-            png_path = os.path.join(output_folder, png_file)
-            image.save(png_path)
+                # Set the resolution (DPI) for the image
+                resolution = 300  # You can adjust this as needed
+                pixmap = page.get_pixmap(matrix=fitz.Matrix(resolution/72, resolution/72))
 
-        # Close the PDF file
-        pdf_document.close()
+                # Convert the Pixmap to a Pillow Image
+                image = Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
 
-print("Conversion complete.")
+                # Set the quality factor (0-100). A higher value results in better quality and larger file size.
+                quality = 95  # You can adjust this as needed
+
+                # Save the image with the specified quality
+                png_file = os.path.splitext(filename)[0] + f'_page{page_num + 1}.png'
+                png_path = os.path.join(output_folder, png_file)
+                image.save(png_path, 'PNG', quality=quality)
+
+            # Close the PDF file
+            pdf_document.close()
+
+    print("PDF conversion complete.")
