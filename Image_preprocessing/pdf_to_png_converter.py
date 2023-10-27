a7d16bcd2e317b13106ab9442901629a2a24480d
Siverty <justinschots13@gmail.com>
2023-10-27 11:41:28 +0200

    I don't even know anymore

--- Image_preprocessing/pdf_to_png_converter.py
+++ Image_preprocessing/pdf_to_png_converter.py
@@ -1,54 +1,31 @@
-# # Install pdf2image: pip install pdf2image
-# import subprocess
-# try:
-#     subprocess.check_call(["pip", "install", "pdf2image"])
-# except subprocess.CalledProcessError as e:
-#     print(f"Error: {e}")
-
-# General imports 
-import fitz  # PyMuPDF
 import os
+import subprocess
 import shutil
 
-check_for_pdf_in_folder = "/test/Original_PID"
-copy_folder = "/test/Original_PID_PDF"
+check_for_pdf_in_folder = "test/Original_PID"
+copy_folder = "test/Original_PID_PDF"
 
-def pdf_to_png(pdf_file, output_directory):
-    # Open the PDF file
-    pdf_document = fitz.open(pdf_file)
+pdf_files = [f for f in os.listdir(check_for_pdf_in_folder) if f.endswith('.pdf')]
+for pdf_file in pdf_files:
+    shutil.move(os.path.join(check_for_pdf_in_folder, pdf_file), copy_folder)
 
-    # Iterate through each page and convert it to PNG
-    for page_number in range(len(pdf_document)):
-        page = pdf_document[page_number]
-        image_list = page.get_pixmap()
-        image_file = f"{output_directory}/page_{page_number + 1}.png"
+def pdf_to_png_converter(check_for_pdf_in_folder, copy_folder):
+    pdf_files = [f for f in os.listdir(check_for_pdf_in_folder) if f.endswith('.pdf')]
 
-        # Save the page as a PNG image
-        image_list.save(image_file, "png")
+    for pdf_file in pdf_files:
+        # Use Ghostscript to convert the PDF to PNG
+        output_folder = os.path.join(check_for_pdf_in_folder, pdf_file[:-4])
+        os.makedirs(output_folder, exist_ok=True)
+        cmd = [
+            "gs",
+            "-dSAFER",
+            "-dBATCH",
+            "-dNOPAUSE",
+            "-sDEVICE=png16m",
+            "-r300",  # Set the resolution (adjust as needed)
+            f"-sOutputFile={output_folder}/%03d.png",  # Output file format
+            os.path.join(check_for_pdf_in_folder, pdf_file)
+        ]
+        subprocess.run(cmd)
 
-    # Close the PDF file
-    pdf_document.close()
-
-def convert_and_move_pdfs(check_for_pdf_in_folder, copy_folder):
-    # Ensure the output directory exists
-    os.makedirs(copy_folder, exist_ok=True)
-
-    # List all files in the check folder
-    files = os.listdir(check_for_pdf_in_folder)
-
-    for file in files:
-        if file.endswith(".pdf"):
-            pdf_file_path = os.path.join(check_for_pdf_in_folder, file)
-            png_output_dir = os.path.join(copy_folder, os.path.splitext(file)[0])
-
-            # Convert the PDF to PNG
-            pdf_to_png(pdf_file_path, png_output_dir)
-
-            # Move the PDF file to the copy folder
-            shutil.move(pdf_file_path, os.path.join(copy_folder, file))
-
-if __name__ == "__main__":
-    check_for_pdf_in_folder = "test/Original_PID"  # Replace with the directory where you want to check for PDFs
-    copy_folder = "test/Original_PID_PDF"  # Replace with the directory where you want to move the PDFs
-
-    convert_and_move_pdfs(check_for_pdf_in_folder, copy_folder)
+pdf_to_png_converter(check_for_pdf_in_folder, copy_folder)
