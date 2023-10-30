13f8d67c310d262d7d544f6399cce720cdf73ad1
Siverty <justinschots13@gmail.com>
2023-10-30 15:21:28 +0100

    converts any .jpg files to .png files in val folder now as well!

--- Image_preprocessing/PreProcessing.py
+++ Image_preprocessing/PreProcessing.py
@@ -9,6 +9,7 @@ import os
 
 # Set train and label folder paths
 train_folder = './datasets/Labels_PID/images/train'
+val_folder = './datasets/Labels_PID/images/val'
 label_folder = './datasets/Labels_PID/labels/train'
 input_folder = 'test\P&ID_input(place_pdf_here)'
 output_folder = 'test\Original_PID_PNG'
@@ -19,7 +20,7 @@ for filename in os.listdir(train_folder):
     pdf_to_png_converter(input_folder, output_folder)
 
     # Convert any image to png
-    img = any_to_png_converter(train_folder)
+    img = any_to_png_converter(train_folder, val_folder)
 
     # Apply blur
     img = blur_images(train_folder, label_folder)
--- Image_preprocessing/to_png_converter.py
+++ Image_preprocessing/to_png_converter.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-def any_to_png_converter(train_folder):
+def any_to_png_converter(train_folder, val_folder):
     for filename in os.listdir(train_folder):
         if not filename.endswith(".png"):
             print("converting " + filename + " to png")
@@ -9,5 +9,13 @@ def any_to_png_converter(train_folder):
             new_filename = os.path.splitext(filename)[0] + ".png"
             img.save(os.path.join(train_folder, new_filename))
             os.remove(os.path.join(train_folder, filename))
+
+    for filename in os.listdir(val_folder):
+        if not filename.endswith(".png"):
+            print("converting " + filename + " to png")
+            img = Image.open(os.path.join(val_folder, filename))
+            new_filename = os.path.splitext(filename)[0] + ".png"
+            img.save(os.path.join(val_folder, new_filename))
+            os.remove(os.path.join(val_folder, filename))
 
     print("Conversion and deletion complete.")
